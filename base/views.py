f2c77cb5568f82fe025cacb74908df36180fa6f0
elementalneil <nayek.sankhanil@gmail.com>
2021-12-27 23:30:32 +0530

    Changed Countdown Timer to run Dynamically

--- base/views.py
+++ base/views.py
@@ -10,22 +10,7 @@ participants=[                      #Temporary Context
 ]
 
 def home(request):
-    #Time Calculation Starts Here:
-    event_time=1641389400.0
-    current_time=time.time()
-
-    rem_time=int(event_time-current_time)
-
-    days=rem_time//86400
-    rem_time=rem_time%86400
-    hours=rem_time//3600
-    rem_time=rem_time%3600
-    minutes=rem_time//60
-    seconds=rem_time%60
-    time_dict={'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds}
-    #Time Calculated and Stored
-
-    context={'participants':participants, 'time':time_dict}
+    context={'participants':participants}
     return render(request, 'base/home.html', context)
 
 def contact(request):
