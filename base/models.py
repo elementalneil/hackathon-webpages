1a6b528ed20a309ff6e2836613c4a17495dafd32
elementalneil <nayek.sankhanil@gmail.com>
2021-12-28 00:56:06 +0530

    Added Database that Stores Participant Data
    -Added Database that Stores Participant Data
    -Changed views and template for homepage to display participant data from the homepage.
    -Added an admin account to modify database

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Participant(models.Model):
+    name = models.CharField(max_length=200)
+    dob = models.DateField()
+    roll = models.CharField(max_length=9)
+    branch = models.CharField(max_length=4)
+    mobile = models.CharField(max_length=13)
+    email = models.CharField(max_length=50)
+    sex = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import time
+from .models import Participant
 
 # Create your views here.
-participants=[                      #Temporary Context
-    {'id':1, 'name':'John Doe'},
-    {'id':2, 'name':'Jane Doe'},
-    {'id':3, 'name':'Jack Doe'},
-]
+# participants=[                      #Temporary Context
+#     {'id':1, 'name':'John Doe'},
+#     {'id':2, 'name':'Jane Doe'},
+#     {'id':3, 'name':'Jack Doe'},
+# ]
+
+participants = Participant.objects.all()
 
 def home(request):
     context={'participants':participants}
