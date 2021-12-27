a2ed5db4de6685286426b8f88444850155f600e8
elementalneil <nayek.sankhanil@gmail.com>
2021-12-27 14:07:41 +0530

    Created Static Countdown Timer on Homepage

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 # Create your views here.
 participants=[                      #Temporary Context
@@ -9,7 +10,22 @@ participants=[                      #Temporary Context
 ]
 
 def home(request):
-    context={'participants':participants}
+    #Time Calculation Starts Here:
+    event_time=1641389400.0
+    current_time=time.time()
+
+    rem_time=int(event_time-current_time)
+
+    days=rem_time//86400
+    rem_time=rem_time%86400
+    hours=rem_time//3600
+    rem_time=rem_time%3600
+    minutes=rem_time//60
+    seconds=rem_time%60
+    time_dict={'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds}
+    #Time Calculated and Stored
+
+    context={'participants':participants, 'time':time_dict}
     return render(request, 'base/home.html', context)
 
 def contact(request):
