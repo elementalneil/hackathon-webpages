d080273450636e827466eefeb60f9c622fc49112
elementalneil <nayek.sankhanil@gmail.com>
2021-12-28 02:32:41 +0530

    Created Registration Form
    -Users can Register from the registration form.
    -Registered User Details will be stored to the database.
    -No client-side data validation added yet.

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact-us/', views.contact, name='contact'),
+    path('register/', views.register, name='register')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Participant
+from .forms import ParticipantForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 # participants=[                      #Temporary Context
@@ -9,11 +11,32 @@ from .models import Participant
 #     {'id':3, 'name':'Jack Doe'},
 # ]
 
-participants = Participant.objects.all()
-
 def home(request):
+    participants = Participant.objects.all()
     context={'participants':participants}
     return render(request, 'base/home.html', context)
 
 def contact(request):
     return render(request, 'base/contact.html')
+
+def register(request):
+    form = ParticipantForm()
+
+    # p=Participant(name='JD', dob='1997-01-01', roll='M156487CA', branch='BTech', mobile='+911546567251', email='jd@gmail.com', sex='F')
+    # p.save()
+    if request.method == 'POST':
+        p=Participant()
+        p.name=request.POST.get('name')
+        p.dob=request.POST.get('dob')
+        p.roll=request.POST.get('roll')
+        p.branch=request.POST.get('branch')
+        p.mobile=request.POST.get('mobile')
+        p.email=request.POST.get('email')
+        p.sex=request.POST.get('sex')
+        p.save()
+        return redirect('home')
+    #     # form = ParticipantForm(request.POST)
+    #     # if form.is_valid():
+    #     #     form.save()
+
+    return render(request, 'base/form.html')
