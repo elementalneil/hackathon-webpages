91f1ca316ed361e2bf645edc6f9060f6147f49ed
elementalneil <nayek.sankhanil@gmail.com>
2021-12-27 12:58:19 +0530

    Modified Homepage to display registered users, with a temporary list of registered users

--- base/views.py
+++ base/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+participants=[                      #Temporary Context
+    {'id':1, 'name':'John Doe'},
+    {'id':2, 'name':'Jane Doe'},
+    {'id':3, 'name':'Jack Doe'},
+]
+
 def home(request):
-    return render(request, 'base/home.html')
+    context={'participants':participants}
+    return render(request, 'base/home.html', context)
 
 def contact(request):
     return render(request, 'base/contact.html')
